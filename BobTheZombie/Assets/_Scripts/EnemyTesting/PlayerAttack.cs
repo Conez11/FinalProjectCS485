122dd84d1e0af38c7a09642a59cf5045c380f253
Andrew D <ardunn11@gmail.com>
2017-10-30 09:02:14 -0700

    Obstical Generation, Player Health and Attack, Rooms

---
+++ BobTheZombie/Assets/_Scripts/EnemyTesting/PlayerAttack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAttack : MonoBehaviour {
+	public GameObject Enemy;
+	
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) && GetComponent<BoxCollider>())
+		{
+			Destroy (Enemy);
+		}
+	}
+}
